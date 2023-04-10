b127f523386b649bc98a855c71d6b088692e8264
unknown <gownideepu23@gmail.com>
2023-04-10 09:21:02 +0530

    added

--- src/main/java/com/GreetinApp/GreetingAppDevelopment/Controller/MyController.java
+++ src/main/java/com/GreetinApp/GreetingAppDevelopment/Controller/MyController.java
@@ -6,13 +6,26 @@ import com.GreetinApp.GreetingAppDevelopment.Repository.MyRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class MyController {
     @Autowired
     private MyRepo myRepo;
-
+    public List<MyModel>getalldata(){
+        List<MyModel>data=new ArrayList<>();
+        myRepo.findAll().forEach(datas ->data.add(datas));
+        return data;
+    }
+    public void save(MyModel data){
+        myRepo.save(data);
+    }
+    @GetMapping("/get")
+    public List<MyModel>getall(){
+        return getalldata();
+    }
     @PostMapping("/hello")
     public MyModel addEmployee(@RequestBody MyDto myDto){
         MyModel data=new MyModel(myDto);
@@ -23,4 +36,14 @@ public class MyController {
     public String greeting() {
         return "Hello world";
     }
+    @GetMapping("/{id}")
+    public String getEmployee(@PathVariable long id) {
+        Optional<MyModel> data = myRepo.findById(id);
+        String greeting = "data is not found";
+        if (data.isPresent()) {
+            greeting = data.get().getFirstName() + data.get().getLastName() + data.get().getGreeting();
+        }
+        return greeting;
+    }
+
 }
